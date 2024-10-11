aa45140e0d4973466957f46420ca1eac19342611
Artis Pencis <artis.ivo@gmail.com>
2024-10-11 14:54:57 +0300

    Improved test data and input

--- Program.cs
+++ Program.cs
@@ -1,50 +1,105 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 
 // Initialize test data
 var incomeData = LoadJson<List<Dictionary<string, int?>>>("./test-data/income.json");
 var travelData = LoadJson<Dictionary<string, Dictionary<string, int?>>>("./test-data/travel.json");
+var stopwatch = new Stopwatch();
+int weekends = 52;
 
-const int weekends = 52;
-var initialSolution = new string[weekends];
-initialSolution[0] = "Bahrain";
-initialSolution[1] = "Belgium";
-initialSolution[2] = "Suzuka";
-initialSolution[3] = "Baku";
-initialSolution[4] = "Saudi Arabia";
-initialSolution[5] = "Australia";
-initialSolution[6] = "China";
-initialSolution[7] = "Miami";
-initialSolution[8] = "Emilia-Romagna";
-initialSolution[9] = "Monaco";
-initialSolution[10] = "Canada";
-initialSolution[11] = "Spain";
-initialSolution[12] = "Austria";
-initialSolution[13] = "Great Britain";
-initialSolution[14] = "Hungary";
-initialSolution[15] = "Netherlands";
-initialSolution[16] = "Italy";
-initialSolution[17] = "Singapore";
-initialSolution[18] = "Mexico";
-initialSolution[19] = "Brazil";
-initialSolution[20] = "Las Vegas";
-initialSolution[21] = "Qatar";
-initialSolution[22] = "Abu Dhabi";
-
-var result = TabuSearch(initialSolution, 1000, 10);
-
-Console.WriteLine("Best schedule found:");
-for (int i = 0; i < result.Length; i++)
+while (true)
 {
-    Console.WriteLine($"Weekend {i}: {result[i] ?? "free"}");
+    stopwatch.Reset();
+    var initialSolution = new string?[weekends];
+    initialSolution[0] = "Bahrain";
+    initialSolution[1] = "Belgium";
+    initialSolution[2] = "Suzuka";
+    initialSolution[3] = "Baku";
+    initialSolution[4] = "Saudi Arabia";
+    initialSolution[5] = "Australia";
+    initialSolution[6] = "China";
+    initialSolution[7] = "Miami";
+    initialSolution[8] = "Emilia-Romagna";
+    initialSolution[9] = "Monaco";
+    initialSolution[10] = "Canada";
+    initialSolution[11] = "Spain";
+    initialSolution[12] = "Austria";
+    initialSolution[13] = "Great Britain";
+    initialSolution[14] = "Hungary";
+    initialSolution[15] = "Netherlands";
+    initialSolution[16] = "Italy";
+    initialSolution[17] = "Singapore";
+    initialSolution[18] = "Mexico";
+    initialSolution[19] = "Brazil";
+    initialSolution[20] = "Las Vegas";
+    initialSolution[21] = "Qatar";
+    initialSolution[22] = "Abu Dhabi";
+
+
+    Console.WriteLine("Please provide maximum iteration count. (Skip with 'Enter', default of 1000 will be used)");
+    if (!int.TryParse(Console.ReadLine(), out int maxIterations))
+    {
+        maxIterations = 1000;
+    };
+
+    Console.WriteLine("Please provide tabu list size. (Skip with 'Enter', default of 10 will be used)");
+    if (!int.TryParse(Console.ReadLine(), out int tabuListSize))
+    {
+        tabuListSize = 10;
+    };
+
+    Console.WriteLine("Please provide comma separated list of ids for Grand Prix to include");
+    for (int i = 0; i < 23; i++)
+    {
+        Console.WriteLine($"{i}: {initialSolution[i]}");
+    }
+
+    string? list = Console.ReadLine();
+
+    if (list != null)
+    {
+        var includeList = list.Split(',');
+        List<int> idList = new List<int>();
+        foreach (var item in includeList)
+        {
+            if (int.TryParse(item, out int id))
+            {
+                idList.Add(id);
+            }
+        }
+
+        if (idList.Any())
+        {
+            for (int i = 0; i < 23; i++)
+            {
+                if (!idList.Contains(i))
+                {
+                    initialSolution[i] = null;
+                }
+            }
+        }
+    }
+
+
+    stopwatch.Start();
+    var result = await Task.Run(() => TabuSearch(initialSolution, maxIterations, tabuListSize));
+    stopwatch.Stop();
+    Console.WriteLine("Best schedule found:");
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.WriteLine($"Weekend {i}: {result[i] ?? "free"}");
+    }
+
+    Console.WriteLine($"Value: ${CalculateSchedulesValue(result)} milions");
+    Console.WriteLine($"Search time: {stopwatch.ElapsedTicks} ms");
 }
 
-Console.WriteLine($"Value: {CalculateSchedulesValue(result)}");
 
 // Neighborhood function
-static List<string[]> GetNeighbors(string[] solution)
+List<string?[]> GetNeighbors(string?[] solution)
 {
-    List<string[]> neighbors = new List<string[]>();
+    List<string?[]> neighbors = new List<string?[]>();
     for (int i = 0; i < solution.Length; i++)
     {
         for (int j = i + 1; j < solution.Length; j++)
@@ -52,7 +107,7 @@ static List<string[]> GetNeighbors(string[] solution)
             // Empty weekends doesn't impact anything.
             if (solution[j] == null && solution[i] == null) continue;
 
-            var neighbor = new string[weekends];
+            var neighbor = new string?[weekends];
             solution.CopyTo(neighbor, 0);
 
             // Swaps weekends
@@ -80,20 +135,20 @@ int GetTravelCost(string grandPrix, string nextGrandPrix)
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
 }
 
-int CalculateSchedulesValue(string[] solution)
+int CalculateSchedulesValue(string?[] solution)
 {
     int result = 0;
     for (int i = 0; i < solution.Length; i++)
     {
         if (solution[i] == null) continue;
 
-        result += GetIncome(solution[i], i);
+        result += GetIncome(solution[i]!, i);
 
         for (int j = i + 1; j < solution.Length; j++)
         {
             if (solution[j] != null)
             {
-                result -= GetTravelCost(solution[i], solution[j]);
+                result -= GetTravelCost(solution[i]!, solution[j]!);
                 break;
             }
         }
@@ -102,24 +157,24 @@ int CalculateSchedulesValue(string[] solution)
 }
 
 // Tabu Search Algorithm
-string[] TabuSearch(string[] initialSolution,
+Task<string?[]> TabuSearch(string?[] initialSolution,
                     int maxIterations,
                     int tabuListSize)
 {
 
     // Initialize tabu search
-    string[] bestSolution = initialSolution;
-    string[] currentSolution = initialSolution;
-    List<string[]> tabuList = new List<string[]>();
+    string?[] bestSolution = initialSolution;
+    string?[] currentSolution = initialSolution;
+    List<string?[]> tabuList = new List<string?[]>();
     var comparer = new CompareSchedules();
 
     for (int iter = 0; iter < maxIterations; iter++)
     {
-        List<string[]> neighbors = GetNeighbors(currentSolution);
-        string[]? bestNeighbor = null;
+        List<string?[]> neighbors = GetNeighbors(currentSolution);
+        string?[]? bestNeighbor = null;
         // We are interested in most profit.
         int bestNeighborFitness = -1;
-        foreach (string[] neighbor in neighbors)
+        foreach (string?[] neighbor in neighbors)
         {
             if (!tabuList.Contains(neighbor, comparer))
             {
@@ -152,7 +207,7 @@ string[] TabuSearch(string[] initialSolution,
             bestSolution = bestNeighbor;
         }
     }
-    return bestSolution;
+    return Task.FromResult(bestSolution);
 }
 
 static T? LoadJson<T>(string fileLocation)
@@ -164,9 +219,9 @@ static T? LoadJson<T>(string fileLocation)
     }
 }
 
-class CompareSchedules : IEqualityComparer<string[]>
+class CompareSchedules : IEqualityComparer<string?[]>
 {
-    public bool Equals(string[]? x, string[]? y)
+    public bool Equals(string?[]? x, string?[]? y)
     {
         if (x == null || y == null) return false;
         if (x.Length != y.Length) return false;
@@ -179,7 +234,7 @@ class CompareSchedules : IEqualityComparer<string[]>
         return true;
     }
 
-    public int GetHashCode([DisallowNull] string[] obj)
+    public int GetHashCode([DisallowNull] string?[] obj)
     {
         return 0;
     }
